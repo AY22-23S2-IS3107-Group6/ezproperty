aa4b8d2907958e796844e1a488c50ccecab1ff13
hongpeigoh <57928283+hongpeigoh@users.noreply.github.com>
2023-03-16 20:28:34 +0800

    Add Main PropertyTransaction

--- db/warehouse/schemas/__init__.py
+++ db/warehouse/schemas/__init__.py
@@ -1,4 +1,5 @@
+from .main import main_create, main_insert
 from .amn import amn_create, amn_insert
 
-create_queries = amn_create
-insert_queries = amn_insert
+create_queries = main_create + amn_create
+insert_queries = main_insert + amn_insert
