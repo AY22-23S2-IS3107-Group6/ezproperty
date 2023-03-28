5bd89effaf01e37fae31f56c587c2835569c0340
Sean Tan <seantan1218@gmail.com>
2023-03-28 15:27:17 +0800

    Added rentalproject and rentalmedian schema and baseline etl

---
+++ db/etl/rentalTransactions.py
@@ -0,0 +1,111 @@
+import pymongo
+import pandas as pd
+import requests
+from ..lake import DataLake
+from ..warehouse import DataWarehouse
+
+
+# Feed data into lake / mongoDB
+def extract():
+
+    print("Rental Transactions: Feeding data into lake")
+
+    URA_API_ACCESSKEY = '84d7c27b-8dc6-4a34-9ea3-7769c174007c'
+
+    # Fetches daily token which is needed along with access key for API calls
+    fetchTokenHeader = {'Content-Type': 'application/json', 'AccessKey': URA_API_ACCESSKEY, 'Accept': 'application/json', 'User-Agent': 'PostmanRuntime/7.28.4'} # returns html if i dont include user agent postman
+    resp = requests.get('https://www.ura.gov.sg/uraDataService/insertNewToken.action', headers = fetchTokenHeader) 
+
+    print(resp)
+    print(resp.json())
+
+    URA_API_TOKEN = resp.json()['Result']
+
+    # Setting up default header for API calls
+    apiHeader = {'Content-Type': 'application/json', 'AccessKey': URA_API_ACCESSKEY, 'Token': URA_API_TOKEN, 'User-Agent': 'PostmanRuntime/7.30.1'}
+
+    # Fetch both data sets
+    resp = requests.get('https://www.ura.gov.sg/uraDataService/invokeUraDS?service=PMI_Resi_Rental_Median', headers = apiHeader)
+
+    rentalTransactions = resp.json()['Result']
+
+    # Insert data
+    db = DataLake()
+    db.insert_to_schema("main__RentralTransactions", rentalTransactions) # no logic currently to manage reloading data, so uncomment this after populating db
+
+    # Test query
+    testResult = db.query_find("main__RentralTransactions", 
+        { "project": "ELLIOT AT THE EAST COAST" }
+    )
+
+    # Proof that query works
+    for x in testResult:
+        print(x)
+
+    # Query to get data - not super needed since currently fetching all, but just in case want to modify query
+    result = db.query_find("main__RentralTransactions", 
+        {}
+    )
+    
+    transform(result)
+
+
+# Transform data accordingly
+def transform(result):
+   
+    print("Rental Transactions: Transforming data")
+
+    temp = list(result)
+    rentalProject = []
+
+    # Remove projects with missing data
+    for project in temp:
+        if ('rentalMedian' in project) and ('street' in project) and ('x' in project) and ('project' in project) and ('y' in project):
+            if project['rentalMedian'] != []:
+                rentalProject.append(project)
+
+    # Typecast appropriately to feed into sql
+    for project in rentalProject:
+        project['x'] = float(project['x'])
+        project['y'] = float(project['y'])
+
+    mediansOnly = []
+
+    # Break into two tables
+    for project in rentalProject:
+
+        # this will be used as the rental project ID
+        project['_id'] = id(project['_id']) # using python generated _id for now since cant find a suitable pkey
+
+        tempMedians = project['rentalMedian']
+
+        # Bring out all the rental medians (flatten) into a new json object
+        for median in tempMedians:
+            mediansOnly.append(median)
+            # add foreign key linking to project
+            median['rentalProject'] = project['_id']
+
+        del project['rentalMedian']
+
+    print(rentalProject[0])
+    print(mediansOnly[0])
+
+    load(rentalProject, mediansOnly)
+
+
+# Load data into MySQL accordingly
+def load(rentalProject, medianOnly):
+
+    print("Rental Transactions: Loading data")
+
+    # # Transform data to list of values
+    # result = list(map(lambda x: tuple(x.values()), result))
+
+    # # Insert data
+    # db = DataWarehouse()
+    # db.insert_to_schema("main__RentralTransactions", result)
+
+
+extract()
+
+
--- db/warehouse/schemas/main.py
+++ db/warehouse/schemas/main.py
@@ -45,47 +45,84 @@ main_insert['main__PropertyTransaction'] = ('''
     VALUES
     (%d, %s, %s, %d, %d, %s, %d, %d, %d, %s, %d, %s, %d, %d)
 ''')
-
-main_create['main__RentalTransaction'] = ('''
-    CREATE TABLE `main__RentalTransaction` (
-    id              int             AUTO_INCREMENT,
-    district        int             NOT NULL,
+                                            
+main_create['main__RentalProject'] = ('''
+    CREATE TABLE `main__RentalProject` (
+    _id             varchar(24)     NOT NULL,
     street          varchar(100)    NOT NULL,
-    block           varchar(50)     NOT NULL,
-    propertyType    enum(
-        'STRATA DETACHED',
-        'STRATA SEMIDETACHED',
-        'STRATA TERRACE',
-        'DETACHED',
-        'SEMI-DETACHED',
-        'TERRACE',
-        'APARTMENT',
-        'CONDOMINIUM',
-        'EXECUTIVE CONDOMINIUM',
-        '1 ROOM HDB',
-        '2 ROOM HDB',
-        '3 ROOM HDB',
-        '4 ROOM HDB',
-        '5 ROOM HDB',
-        'EXECUTIVE HDB',
-        'MULTI-GENERATION HDB'
-    )                               NOT NULL,
-    noOfRoom        int                     ,
-    area            int             NOT NULL,
-    rent            decimal(12,2)   NOT NULL,
-    psf25           decimal(12,2)           ,
-    median          decimal(12,2)           ,
-    psf75           decimal(12,2)           ,
-    transactionDate date                    ,
     x               decimal(9,4)    NOT NULL,
+    project         varchar(100)    NOT NULL,   
     y               decimal(9,4)    NOT NULL,
-    PRIMARY KEY (id)
+    PRIMARY KEY (_id)
 )
 ''')
 
-main_insert['main__RentalTransaction'] = ('''
-    INSERT INTO `main__RentalTransaction`
-    (district, street, block, propertyType, noOfRoom, area, rent, psf25, median, psf75, transactionDate, x, y) 
+main_insert['main__RentalProject'] = ('''
+    INSERT INTO `main__RentalProject`
+    (_id, street, x, project, y) 
     VALUES
-    (%d, %s, %s, %s, %d, %d, %d, %d, %d, %d, %s, %d, %d)
+    (%s, %s, %s, %s, %s)
 ''')
+
+main_create['main__RentalMedian'] = ('''
+    CREATE TABLE `main__RentalMedian` (
+    refPeriod       varchar(24)     NOT NULL,
+    psf75           decimal(12,2)   NOT NULL,
+    median          decimal(12,2)   NOT NULL,
+    psf25           decimal(12,2)   NOT NULL,   
+    district        varchar(10)     NOT NULL
+    rentalProjectId varchar(24)     NOT NULL,
+    PRIMARY KEY (refPeriod, rentalProjectId)
+)
+''')
+
+main_insert['main__RentalMedian'] = ('''
+    INSERT INTO `main__RentalMedian`
+    (refPeriod, psf75, median, psf25, district, rentalProjectId) 
+    VALUES
+    (%s, %s, %s, %s, %s, %s)
+''')
+
+# main_create['main__RentalTransaction'] = ('''
+#     CREATE TABLE `main__RentalTransaction` (
+#     id              int             AUTO_INCREMENT,
+#     district        int             NOT NULL,
+#     street          varchar(100)    NOT NULL,
+#     block           varchar(50)     NOT NULL,
+#     propertyType    enum(
+#         'STRATA DETACHED',
+#         'STRATA SEMIDETACHED',
+#         'STRATA TERRACE',
+#         'DETACHED',
+#         'SEMI-DETACHED',
+#         'TERRACE',
+#         'APARTMENT',
+#         'CONDOMINIUM',
+#         'EXECUTIVE CONDOMINIUM',
+#         '1 ROOM HDB',
+#         '2 ROOM HDB',
+#         '3 ROOM HDB',
+#         '4 ROOM HDB',
+#         '5 ROOM HDB',
+#         'EXECUTIVE HDB',
+#         'MULTI-GENERATION HDB'
+#     )                               NOT NULL,
+#     noOfRoom        int                     ,
+#     area            int             NOT NULL,
+#     rent            decimal(12,2)   NOT NULL,
+#     psf25           decimal(12,2)           ,
+#     median          decimal(12,2)           ,
+#     psf75           decimal(12,2)           ,
+#     transactionDate date                    ,
+#     x               decimal(9,4)    NOT NULL,
+#     y               decimal(9,4)    NOT NULL,
+#     PRIMARY KEY (id)
+# )
+# ''')
+
+# main_insert['main__RentalTransaction'] = ('''
+#     INSERT INTO `main__RentalTransaction`
+#     (district, street, block, propertyType, noOfRoom, area, rent, psf25, median, psf75, transactionDate, x, y) 
+#     VALUES
+#     (%d, %s, %s, %s, %d, %d, %d, %d, %d, %d, %s, %d, %d)
+# ''')
