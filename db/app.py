955581b3d6d76694a6f6773af62f8ee12794c774
hongpeigoh <57928283+hongpeigoh@users.noreply.github.com>
2023-04-09 21:49:50 +0800

    update

--- db/app.py
+++ db/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request
+from flask import Flask, json, jsonify, request
 from flask_cors import CORS
 from db import DataWarehouse
 from db.warehouse.schemas import create_queries
@@ -6,23 +6,36 @@ from db.warehouse.schemas import create_queries
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/<category>/<schema>', methods=['GET'])
-def getData(category: str, schema: str):
-    if f"{category}__{schema}" not in [schema_name.lower() for schema_name in create_queries]:
-        return "Bad request", 400
-    warehouse = DataWarehouse()
-    return jsonify(warehouse.query(f"SELECT * FROM {category}__{schema}"))
+
+@app.route('/<category>/<schema_name>', methods=['GET'])
+def get_data(category: str, schema_name: str):
+    schema = get_schema(category, schema_name)
+    if not schema:
+        return jsonify({"error": "Bad request"}), 400
+    dw = DataWarehouse()
+    return jsonify(dw.query(f"SELECT * FROM {schema}"))
 
 
-@app.route('/<category>/<schema>', methods=['POST'])
-def createData(category: str, schema: str):
-    if f"{category}__{schema}" not in [schema_name.lower() for schema_name in create_queries]:
-        return "Content-Type not supported!", 400
+@app.route('/<category>/<schema_name>', methods=['POST'])
+def post_data(category: str, schema_name: str):
+    schema = get_schema(category, schema_name)
+    if not schema:
+        return jsonify({"error": "Bad request"}), 400
     if request.headers.get('Content-Type') != 'application/json':
-        return "Bad request", 400
+        return jsonify({"error": "Content-Type not supported!"}), 400
     try:
-        warehouse = DataWarehouse()
-        warehouse.insert_to_schema(f"{category}__{schema}", request.json)
+        dw = DataWarehouse()
+        data = request.get_json()
+        if not isinstance(data, list):
+            return jsonify({"error": "JSON data must be a list"}), 400
+        dw.insert_to_schema(f"{schema}", data)
+        return "Successfully added"
     except:
         return "Request failed", 500
-    
+
+
+def get_schema(category: str, schema_name: str):
+    for schema in create_queries:
+        if f"{category}__{schema_name}" == schema.lower():
+            return schema
+    return None
